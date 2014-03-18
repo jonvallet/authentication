218918aa5e337be88888c4903710f1acf01501bf
jonvallet <j.vallet@gmail.com>
2014-03-18 12:56:40 +0100

    Added thread support to the cracker.

--- src/main/java/com/jonvallet/authentication/SaltedHashCracker.java
+++ src/main/java/com/jonvallet/authentication/SaltedHashCracker.java
@@ -5,32 +5,57 @@ import java.util.Date;
 /**
  * Created by jvalletw on 5/03/14.
  */
-public class SaltedHashCracker {
+public class SaltedHashCracker extends Thread {
 
 
-    public static void main (String [] argsv){
 
+    private int start;
+    private int jump;
+    private String hash;
+    private String salt;
+
+    public SaltedHashCracker(int start, int jump, String hash, String salt){
+        this.start = start;
+        this.jump = jump;
+        this.hash = hash;
+        this.salt = salt;
+    }
+
+    public void run (){
         Hasher hasher = new Hasher();
-
-        String hashToBreak = "49C2D840522EA37576109CB6F69BFBC33723230A116FA1024557DBF6F6FF0EE9";
-        String salt = "802863448282462273";
-        System.out.println("Trying to crack: "+hashToBreak);
 
         Date date = new Date();
 
-        int i = 0;
-        String testUser;
+        String testUser = null;
         String calculatedHash;
 
-        do{
+        for (int i = start; i < 100000000; i = i + jump ){
             testUser = String.format("%08d", i);
             calculatedHash = hasher.hashSha_2(testUser+salt);
-            i++;
-        }while (!hashToBreak.equals(calculatedHash));
-        
-        Date date2 = new Date();
-        date2.setTime(date2.getTime()-date.getTime());
-        System.out.println("User found: "+testUser +" in " +date2.getTime()/1000+" seconds");
+            if (hash.equals(calculatedHash)){
+                Date date2 = new Date();
+                date2.setTime(date2.getTime()-date.getTime());
+                System.out.println("User found: "+testUser +" in " +date2.getTime()/1000+" seconds");
+                break;
+            }
+        }
+
+
+
+    }
+
+    public static void main (String [] argsv){
+
+        String hashToBreak = "49C2D840522EA37576109CB6F69BFBC33723230A116FA1024557DBF6F6FF0EE9";
+        String salt = "802863448282462273";
+
+        System.out.println("Trying to crack: "+hashToBreak);
+
+        int threads = 1;
+
+        for (int i = 0; i < threads; i++){
+            new SaltedHashCracker(i, threads, hashToBreak, salt).start();
+        }
 
     }
 
